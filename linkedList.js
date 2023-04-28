481bbc72759f98cd68df798ab87d671bec1b7955
Mateen <zahidx297@yahoo.com>
2023-04-28 03:41:20 -0400

    add append functionality

--- linkedList.js
+++ linkedList.js
@@ -1,12 +1,31 @@
 console.log('Hello, world!');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.head = null;
+    }
+    append(value) {
+        if (!this.head) {
+            this.head = new Node(value);
+            return;
+        }
+        let node = this.head;
+        while (node.next != null)
+            node = node.next;
+        node.next = new Node(value);
+    }
 }
 
 class Node {
-    constructor() {
-        this.value = null;
+    constructor(value = null) {
+        this.value = value;
         this.next = null;
     }
 }
+
+let list = new LinkedList;
+list.append(20);
+list.append(40);
+list.append(60);
+console.log(list);
+
