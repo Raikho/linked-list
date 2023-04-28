e5bc5c740c6f94538c18da0d983b32f0ed85eb89
Mateen <zahidx297@yahoo.com>
2023-04-28 03:53:23 -0400

    add prepend and size functinality

--- linkedList.js
+++ linkedList.js
@@ -1,9 +1,15 @@
-console.log('Hello, world!');
+class Node {
+    constructor(value = null) {
+        this.value = value;
+        this.next = null;
+    }
+}
 
 class LinkedList {
     constructor() {
         this.head = null;
     }
+
     append(value) {
         if (!this.head) {
             this.head = new Node(value);
@@ -14,18 +20,31 @@ class LinkedList {
             node = node.next;
         node.next = new Node(value);
     }
-}
 
-class Node {
-    constructor(value = null) {
-        this.value = value;
-        this.next = null;
+    prepend(value) {
+        let tempNext = this.head;
+        this.head = new Node(value);
+        this.head.next = tempNext;
+    }
+
+    get size() {
+        let node = this.head;
+        if (!node) return 0;
+        let size = 1;
+        while(node.next) {
+            node = node.next;
+            size++;
+        }
+        return size;
     }
 }
 
 let list = new LinkedList;
-list.append(20);
-list.append(40);
-list.append(60);
+list.append(4);
+list.append(6);
+list.append(8);
+list.prepend(2);
+
 console.log(list);
+console.log(list.size);
 
